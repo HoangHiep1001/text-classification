4ee4179d0ef22c1256971dd00e51cdcc9bf5890a
hiephm <hiephoangmanh@admicro.vn>
2021-03-18 17:47:20 +0700

    init

---
+++ bin/install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+pip install cython
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo easy_install numpy
+sudo easy_install scipy
+
+# Install Gensim
+sudo easy_install -U gensim
