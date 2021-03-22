031c698bbb923ac69d044dda53d31fdc93373611
hiephm <hiephoangmanh@admicro.vn>
2021-03-22 14:03:20 +0700

    init

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -7,19 +7,23 @@ from src.preprocess import nlp_utils
 from src.utils.unicode import covert_unicode
 import csv
 
+
 def read_file(filePath):
     list = []
-    with codecs.open(filePath,"r",encoding="utf8") as file:
+    with codecs.open(filePath, "r", encoding="utf8") as file:
         a = file.readlines()
-        for i in a :
+        for i in a:
             list.append(i)
     return list
+
 
 def removeHtml(text):
     return re.sub(r'<[^>]*>', '', text)
 
+
 def unicodeConvert(text):
     return covert_unicode(text)
+
 
 def text_preprocess(text):
     text = unicodeConvert(text)
@@ -34,6 +38,7 @@ def text_preprocess(text):
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
+
 def remove_stopword(text):
     filename = '../../data/stopwords.csv'
     data = pd.read_csv(filename, sep="\t", encoding='utf-8')
@@ -45,28 +50,28 @@ def remove_stopword(text):
             pre_text.append(word)
         text2 = ' '.join(pre_text)
     return text2
-def write_data_process(path,text):
-    with codecs.open(path,"w",encoding="utf8") as file:
-        file.write(text+"\n")
+
+
+def write_data_process(path, text):
+    with codecs.open(path, "w", encoding="utf8") as file:
+        file.write(text + "\n")
+
+
 if __name__ == '__main__':
     path_in = '../../data/data.txt'
     path_rs = "../../data/data_process.csv"
     data = read_file(path_in)
-    df = []
-    for text in data:
-        try:
-            content = text.split("==")[0];
-            label = text.split("==")[1];
-            a = text_preprocess(content)
-            b = text_preprocess(label)
-            df.append(a + "," + b)
-        except:
-            print("No data")
-    with open(path_rs, 'w', newline='',encoding='utf-8') as csvfile:
+
+    with open(path_rs, 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['content', 'category']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for str in df:
-            ct = str.split(",")[0]
-            lb = str.split(",")[1]
-            writer.writerow({'content':''+ct, 'category': ''+lb})
+        for text in data:
+            try:
+                content = text.split("==")[0];
+                label = text.split("==")[1];
+                a = text_preprocess(content)
+                b = text_preprocess(label)
+                writer.writerow({'content': '' + a, 'category': '' + b})
+            except:
+                print("No data")
--- src/preprocess/word2vec.py
+++ src/preprocess/word2vec.py
@@ -1,4 +1,5 @@
 from gensim.models import Word2Vec
+import pandas as pd
 
 # path data
 pathdata = './data/data-test.txt'
@@ -15,11 +16,18 @@ def read_data(path):
 pathModelBin = 'model/w2v.bin'
 pathModelTxt = 'model/w2v.txt'
 
-train_data = read_data(pathdata)
+if __name__ == '__main__':
+    data = pd.read_csv('../../data/data_process.csv', sep=",", encoding='utf-8');
+    train_data = data['content']
 
-model = Word2Vec(train_data, size=100, window=3, min_count=1, sample=0.0001, workers=4, sg=0, negative=10, cbow_mean=1,
-                 iter=5)
+    try:
+        model = Word2Vec(train_data, size=100, window=3, min_count=1, sample=0.0001, workers=4, sg=0, negative=10,
+                         cbow_mean=1,
+                         iter=5)
+        model.wv.save_word2vec_format(pathModelBin, fvocab=None, binary=True)
+        model.wv.save_word2vec_format(pathModelTxt, fvocab=None, binary=False)
+    except:
 
-model.wv.save_word2vec_format(pathModelBin, fvocab=None, binary=True)
-model.wv.save_word2vec_format(pathModelTxt, fvocab=None, binary=False)
-print("\nTrain done saved to ./model folder.")
+        print("error")
+
+    print("\nTrain done saved to ./model folder.")
