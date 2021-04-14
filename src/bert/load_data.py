bb6077768fc1f5e4d8a248bca79d898712ed9734
hiephm <hiephoangmanh@admicro.vn>
2021-04-14 21:12:21 +0700

    test bert done

--- src/bert/load_data.py
+++ src/bert/load_data.py
@@ -1,55 +1,68 @@
 from os import listdir
 import os
-
 import torch
 from keras_preprocessing.sequence import pad_sequences
 from torch.utils.data import TensorDataset, SequentialSampler, DataLoader
 from tqdm import tqdm
-sep = os.sep()
+
+sep = os.sep
 
 
-def read_data(path_file):
-    texts = []
+def make_mask(batch_ids):
+    batch_mask = []
+    for ids in batch_ids:
+        mask = [int(token_id > 0) for token_id in ids]
+        batch_mask.append(mask)
+    return torch.tensor(batch_mask)
+
+
+def read_data(path_raw, classes=[]):
+    content = []
     labels = []
-    #
-    for folder in listdir(path_file):
-        for file in listdir(path_file + sep + folder):
-            with open(path_file + sep + folder + sep + file, 'r', encoding="utf-8") as f:
-                print("read file: " + path_file + sep + folder + sep + file)
+    for folder in listdir(path_raw):
+        for file in listdir(path_raw + sep + folder):
+            with open(path_raw + sep + folder + sep + file, 'r', encoding="utf-8") as f:
+                print("read file: " + path_raw + sep + folder + sep + file)
                 all_of_it = f.read()
                 sentences = all_of_it.split('\n')
-                texts = texts + sentences
-                label = [folder for _ in sentences]
-                labels = labels + label
+                for str in sentences:
+                    content.append(str)
+                for _ in sentences:
+                    labels.append(classes.index(folder))
                 del all_of_it, sentences
-    return texts, labels
+    return content, labels
 
 
-def text2id(tokenizer):
-    texts, label = read_data("")
-    print("TEXT TO IDS")
+def dataloader_from_text(texts=[], labels=[], tokenizer=None, max_len=256, batch_size=16, infer=False):
+    print(texts[0])
+    print(labels[0])
+    # text to id
     ids = []
     for text in tqdm(texts):
         encoded_sent = tokenizer.encode(text)
         ids.append(encoded_sent)
     del texts
-    ids_padded = pad_sequences(ids, maxlen=256, dtype="long", value=0, truncating="post", padding="post")
+    # padding sentences 256 length
+    ids_padded = pad_sequences(ids, maxlen=max_len, dtype="long", value=0, truncating="post", padding="post")
     del ids
+    print(ids_padded)
+
     print("CONVERT TO TORCH TENSOR")
     ids_inputs = torch.tensor(ids_padded)
     del ids_padded
 
-    labels = torch.tensor(label)
+    if not infer:
+        labels = torch.tensor(labels)
 
     print("CREATE DATALOADER")
-    input_data = TensorDataset(ids_inputs, labels)
+    if infer:
+        input_data = TensorDataset(ids_inputs)
+    else:
+        input_data = TensorDataset(ids_inputs, labels)
     input_sampler = SequentialSampler(input_data)
-    dataloader = DataLoader(input_data, sampler=input_sampler, batch_size=16)
 
-    print("len dataloader:", len(dataloader))
+    data_loader = DataLoader(input_data, sampler=input_sampler, batch_size=batch_size)
+
+    print("len data_loader:", len(data_loader))
     print("LOAD DATA ALL DONE")
-    return dataloader
-
-
-if __name__ == '__main__':
-    text2id()
+    return data_loader
